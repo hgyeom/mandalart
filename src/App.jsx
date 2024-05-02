3318751f960cad599ac0dd47e3c62e59b52bb687
hgyeom <rrune3@naver.com>
2024-05-02 15:11:46 +0900

    기본 세팅. 폴더 구조, node-sass 설치

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Nav from './components/Nav/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <section className="mandalart">
+        <></>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,9 @@
+const Nav = () => {
+  return (
+    <div className="nav">
+      <h1 className="title">만다라트</h1>
+    </div>
+  );
+};
+
+export default Nav;
