20acc2776569e66332d5944438c2181f9c9c38fb
hgyeom <rrune3@naver.com>
2024-05-19 16:53:08 +0900

    Feat : Footer 추가
    // Footer.jsx, App.jsx
    footer 추가.
    
    // index.scss, footer.scss
    footer css파일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Nav from './components/Nav/Nav';
 import Mandalart from './components/Mandalart/Mandalart';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Mandalart />
+      <Footer />
     </div>
   );
 }
