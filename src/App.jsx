db1a0e1053a4746fdf97649e685811ecce1fdb94
hgyeom <rrune3@naver.com>
2024-05-19 17:21:27 +0900

    Feat : 스크린샷 저장 기능
    // App.jsx
    스크린샷 저장 기능 구현.
    html2canvas와 file-saver 사용.
    useRef로 참조중인 ref를 캡처, 저장.
    
    // Nav.jsx
    스크린샷 저장 버튼 구현.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import { useRef } from 'react';
+import saveAs from 'file-saver';
+import html2canvas from 'html2canvas';
+
+// components
 import Nav from './components/Nav/Nav';
 import Mandalart from './components/Mandalart/Mandalart';
 import Footer from './components/Footer/Footer';
 
 function App() {
+  const saveRef = useRef(null);
+
+  const handleDownload = async () => {
+    if (!saveRef.current) return;
+
+    try {
+      const div = saveRef.current;
+      const canvas = await html2canvas(div, { scale: 2 });
+      canvas.toBlob((blob) => {
+        if (blob !== null) {
+          saveAs(blob, 'result.png');
+        }
+      });
+    } catch (error) {
+      console.error('Error converting div to image:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <Nav />
+    <div className="App" ref={saveRef}>
+      <Nav handleDownload={handleDownload} />
       <Mandalart />
       <Footer />
     </div>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,8 +1,15 @@
-const Nav = () => {
+const Nav = ({ handleDownload }) => {
   return (
-    <div className="nav">
-      <h1 className="title">만다라트</h1>
-    </div>
+    <>
+      <div className="nav">
+        <h1 className="title">만다라트</h1>
+        <div className="menu">
+          <button className="saveButton" onClick={handleDownload}>
+            저장
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
